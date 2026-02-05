e8aae26044c2c72ed38244c63a28101ca4957675
hs301443 <106682004+hs301443@users.noreply.github.com>
2026-02-05 15:00:26 +0200

    feat: Add Drizzle ORM `admins` table schema and centralize admin schema exports.

--- src/models/schema.ts
+++ src/models/schema.ts
@@ -4,3 +4,4 @@ export * from "./schema/admin/roles";
 export * from "./schema/admin/Student";
 export * from "./schema/admin/parent";
 export * from "./schema/admin/category";
+
---
+++ src/models/schema/admin/techer.ts
@@ -0,0 +1,19 @@
+import { mysqlTable, varchar, char, timestamp, mysqlEnum, json } from "drizzle-orm/mysql-core";
+import { sql } from "drizzle-orm";
+import { roles } from "./roles";
+import { Permission } from "../../../types/custom";
+import { category } from "../../schema";
+
+export const admins = mysqlTable("admins", {
+    id: char("id", { length: 255 }).primaryKey().notNull().default(sql`(uuid())`),
+    name: varchar("name", { length: 255 }).notNull(),
+    email: varchar("email", { length: 255 }).notNull(),
+    phoneNumber: varchar("phone_number", { length: 255 }).notNull(),
+    password: varchar("password", { length: 255 }).notNull(),
+    avatar: varchar("avatar", { length: 500 }),
+    categoryId: char("category_id", { length: 36 }).references(() => category.id),
+    courseId: char("course_id", { length: 36 }).references(() => course.id),
+
+    createdAt: timestamp("created_at").defaultNow(),
+    updatedAt: timestamp("updated_at").defaultNow(),
+});
