24b50ee0201c70da2ce98c024249a178095d5583
basantsaed2 <basantsaed718@gmail.com>
2026-04-12 17:02:11 +0200

    delete dist

--- dist/src/routes/payment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const wallet_1 = require("../controllers/user/wallet");
-const catchAsync_1 = require("../utils/catchAsync");
-const router = (0, express_1.Router)();
-router.get("/paymob/callback", (0, catchAsync_1.catchAsync)(wallet_1.handlePaymobCallback));
-router.post("/paymob/callback", (0, catchAsync_1.catchAsync)(wallet_1.handlePaymobCallback));
-exports.default = router;
--- dist/src/routes/user/Quizzes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const Quizzes_1 = require("../../controllers/user/Quizzes");
-const catchAsync_1 = require("../../utils/catchAsync");
-const router = (0, express_1.Router)();
-router.get("/:quizId/questions", (0, catchAsync_1.catchAsync)(Quizzes_1.getQuizQuestions));
-exports.default = router;
