9bdaa638f6e54f065170f433ce910087193c70b1
mohamed yasser <mohamed.my886@gmail.com>
2026-07-14 15:38:53 +0300

    fix: change students attendance route method from GET to POST

--- src/routes/admin/Session.ts
+++ src/routes/admin/Session.ts
@@ -28,7 +28,7 @@ router.get("/select/lesson/:chapterId", catchAsync(selectLesson));
 router.get("/select/students", catchAsync(selectStudents));
 router.get("/select/teachers", catchAsync(selectTeachers));
 router.get("/select/groups", catchAsync(selectGroups)); 
-router.get("/students-attendance",requirePermission("sessions", "View"), catchAsync(getStudentsCourseAttendance));
+router.post("/students-attendance",requirePermission("sessions", "View"), catchAsync(getStudentsCourseAttendance));
 
 router.get("/", requirePermission("sessions", "View"), catchAsync(getAllSessions));
 router.post("/", requirePermission("sessions", "Add"), catchAsync(createSession));
